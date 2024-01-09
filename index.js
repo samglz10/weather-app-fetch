5080e113214ca4d1cecdea00230c5c535d845d2d
Samuel Gonzalez <samglz10@gmail.com>
2024-01-08 20:23:21 -0500

    successful fetch call

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+require('dotenv').config();
 console.log('weather app')
+
+const BASE_URL='http://api.weatherapi.com/v1/'
+const API_KEY = process.env.API_KEY;
+const LOCATION = 'pawashingtonris'
+
+
+console.log(`${BASE_URL}forecast.json?key=${API_KEY}&q=${LOCATION}`)
+
+function fetchAPI(){
+    fetch(`${BASE_URL}current/json?key=${API_KEY}`, {
+        method: 'GET',
+    })
+    .then((response)=>{
+        response.json()
+    })
+    .then((data)=>{
+        console.log(`this is the ${data}`)
+    })
+    .catch((error)=>{
+        throw new Error("error")
+
+    })
+
+}
+
+fetchAPI();
